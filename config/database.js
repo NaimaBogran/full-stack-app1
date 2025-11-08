7ab2d6c3ceec8efd8312f41949f927af4edbd025
Naima e Bogran <Naima.E.Bogran@gmail.com>
2025-11-08 17:40:20 -0500

    files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://naimaebogran_db_user:CD7xqfRrHC7p3jbd@cluster0.hod0vpl.mongodb.net/?appName=Cluster0', 
+    'dbName': 'demo'
+};
